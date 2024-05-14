5851a6a227c89fb1465bcc7fd611bebe2cfaec51
Illia Novosad <illianovosad@Illias-MacBook-Pro-4.local>
2024-05-14 11:55:11 +0200

    added new explanations to the tutorial.cpp

--- tutorial.cpp
+++ tutorial.cpp
@@ -26,7 +26,7 @@ GitHub(GitLab, BitBucket) is a website where we can put our code on the Internet
 In order to publish code to GitHub, we should create a repository there
 There are instructions on GitHub about how to hook up a folder on a local machine to the repository
 
-git remote add origin 'https link' - hooking up to the website
+git remote add origin 'https link' - hooking up to the website, giving the link a different name(origin)
 git remote origin 'name of the branch' - pushing commit to the website
 
 by default all the code is on the main branch(earlier it was master branch)
@@ -35,6 +35,12 @@ by default all the code is on the main branch(earlier it was master branch)
 git branch 'name of new branch' - creating new branch
 git checkout -b new-branch - creating new branch and going to it
 
+git branch -d 'name of branch' - deleting a branch
+
+
+git push origin 'name of branch' - pushing the changes to GitHub to the branch with that name
+
+git pull origin 'name of branch' - pulling the files from GitHub with the branch name
 
 
 */
