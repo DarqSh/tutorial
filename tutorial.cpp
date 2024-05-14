9daac068f174712c43c9750cd702c32fddfa7ad5
Illia Novosad <illianovosad@Illias-MacBook-Pro-4.local>
2024-05-14 11:37:19 +0200

    modified tutorial.cpp

---
+++ tutorial.cpp
@@ -0,0 +1,43 @@
+#if 1
+/*
+git init - initializes the git
+git add - adding the files which we want to save
+    A file is being saved in the staging area - a file that stores info about what will go to the next commit
+        git add 'specific file'
+        git add .
+
+git commit -m "main message" -m "additional info"
+    -m "additional info" is not necessary
+
+git status - looking at the status of files(e.g. what is added to the next commit)
+
+
+
+git log - history of commits
+
+git checkout 'commit code' - going to the chosen commit. We are at a different branch now
+
+git branch - the list of branches
+
+
+
+
+GitHub(GitLab, BitBucket) is a website where we can put our code on the Internet and other people can work on it, too
+In order to publish code to GitHub, we should create a repository there
+There are instructions on GitHub about how to hook up a folder on a local machine to the repository
+
+git remote add origin 'https link' - hooking up to the website
+git remote origin 'name of the branch' - pushing commit to the website
+
+by default all the code is on the main branch(earlier it was master branch)
+
+
+git branch 'name of new branch' - creating new branch
+git checkout -b new-branch - creating new branch and going to it
+
+
+
+*/
+
+
+#endif
